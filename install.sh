e60730cc9857938138e701fa583f69b27806ab57
itsdalmo <kristian@doingit.no>
2016-09-22 09:40:44 +0200

    Further cleanup and install git hooks

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mkdir -p ~/.config/nvim/
 # Create symbolic links
 ln -sf $DIR/vim/init.vim ~/.vimrc
 ln -sf $DIR/vim/init.vim ~/.config/nvim/init.vim
+ln -sf $DIR/git/gitconfig ~/.gitconfig
 ln -sf $DIR/tmux/tmux.conf ~/.tmux.conf
 ln -sf $DIR/bash/bash_profile ~/.bash_profile
 
@@ -15,15 +16,22 @@ ln -sf $DIR/bash/bash_profile ~/.bash_profile
 mkdir -p ~/.config/nvim/UltiSnips
 SNIPPETS=$(find ${DIR}/vim/UltiSnips/*.snippets -type f | sort);
 for snippet in $SNIPPETS; do
-    echo $snippet ~/.config/nvim/UltiSnips/$( basename $snippet )
-    # ln -sf $snippet ~/.config/nvim/UltiSnips/$( basename $snippet )
+    ln -sf $snippet ~/.config/nvim/UltiSnips/$( basename $snippet )
 done
 
 # Link tmux configs
 mkdir -p ~/.tmux
 TMUXCONF=$(find ${DIR}/tmux/*.config -type f | sort);
 for conf in $TMUXCONF; do
-    echo $conf ~/.tmux/$( basename $conf )
-    # ln -sf $conf ~/.tmux/$( basename $conf )
+    ln -sf $conf ~/.tmux/$( basename $conf )
 done
 
+# Add git hooks
+mkdir -p ~/Telia/
+GITFOLDERS=$(ls -d ~/Telia/*/ );
+for folder in $GITFOLDERS; do
+  if [ -d "$folder/.git" ]; then
+    ln -sf $DIR/git/hooks/pre-push $folder.git/hooks/pre-push
+    ln -sf $DIR/git/hooks/commit-msg $folder.git/hooks/commit-msg
+  fi
+done
