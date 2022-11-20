c9e4b1c4d708d546f09a66fc600430d43e450cd9
itsdalmo <kristian@dalmo.me>
2022-11-20 16:10:45 +0100

    Fix formatting and var name

--- files/.config/nvim/lua/plugins/gitsigns.lua
+++ files/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,31 +1,31 @@
 local present, gitsigns = pcall(require, "gitsigns")
 
 if not present then
-    return
+  return
 end
 
-local config = {
-    signcolumn = true,
-    signs = {
-        add = {
-            hl = "GitSignsAdd",
-            text = "▎",
-            numhl = "GitSignsAddNr",
-            linehl = "GitSignsAddLn"
-        },
-        change = {
-            hl = "GitSignsChange",
-            text = "▎",
-            numhl = "GitSignsChangeNr",
-            linehl = "GitSignsChangeLn"
-        },
-        delete = {
-            hl = "GitSignsDelete",
-            text = "▎",
-            numhl = "GitSignsDeleteNr",
-            linehl = "GitSignsDeleteLn"
-        }
+local options = {
+  signcolumn = true,
+  signs = {
+    add = {
+      hl = "GitSignsAdd",
+      text = "▎",
+      numhl = "GitSignsAddNr",
+      linehl = "GitSignsAddLn"
+    },
+    change = {
+      hl = "GitSignsChange",
+      text = "▎",
+      numhl = "GitSignsChangeNr",
+      linehl = "GitSignsChangeLn"
+    },
+    delete = {
+      hl = "GitSignsDelete",
+      text = "▎",
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn"
     }
+  }
 }
 
-gitsigns.setup(config)
+gitsigns.setup(options)
