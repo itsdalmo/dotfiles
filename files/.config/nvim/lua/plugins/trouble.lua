f0438eb266fcfb256b46778d03136043156436ea
itsdalmo <kristian@dalmo.me>
2023-04-05 22:25:06 +0200

    Remove hack from trouble config

--- files/.config/nvim/lua/plugins/trouble.lua
+++ files/.config/nvim/lua/plugins/trouble.lua
@@ -4,13 +4,6 @@ if not present then
   return
 end
 
--- HACK: https://github.com/folke/trouble.nvim/issues/153
-local util = require("trouble.util")
-local _jump_to_item = util.jump_to_item
-util.jump_to_item = function(win, ...)
-  return _jump_to_item(win or 0, ...)
-end
-
 local options = {}
 
 trouble.setup(options)
