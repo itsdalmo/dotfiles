6e558bb0c7caf2837b5932627094c30024683935
itsdalmo <kristian@dalmo.me>
2023-04-08 14:43:28 +0200

    Replace packer with lazy.nvim

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -1,197 +1,182 @@
-local is_bootstrap = false
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-
-  -- Clone
-  vim.fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-  -- Add
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer = require "packer"
+-- Map leader (must be done right after loading lazy.nvim)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Use popup window for Packer
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float {
-        border = "rounded",
-      }
-    end,
-  },
-}
-
-packer.startup(function(use)
-  use { "wbthomason/packer.nvim" }
-
-  use {
+require('lazy').setup({
+  {
     "nvim-lua/plenary.nvim",
-  }
+  },
 
-  use {
+  {
     "L3MON4D3/LuaSnip",
-  }
+  },
 
-  use {
+  {
     "kylechui/nvim-surround",
     config = function()
       require("nvim-surround").setup({})
     end,
-  }
+  },
 
-  use {
+  {
     "windwp/nvim-autopairs",
     config = function()
       require "plugins.nvim-autopairs"
     end
-  }
+  },
 
-  use {
+  {
     "kyazdani42/nvim-web-devicons",
     config = function()
       require "plugins.nvim-web-devicons"
     end,
-  }
+  },
 
-  use {
+  {
     "folke/tokyonight.nvim",
     config = function()
       require "plugins.tokyonight"
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    build = ":TSUpdate",
     config = function()
       require "plugins.nvim-treesitter"
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-treesitter/nvim-treesitter-textobjects",
-  }
+  },
 
-  use {
+  {
     "kyazdani42/nvim-tree.lua",
     config = function()
       require "plugins.nvim-tree"
     end,
-  }
+  },
 
-  use {
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require "plugins.gitsigns"
     end,
-  }
+  },
 
-  use {
+  {
     "onsails/lspkind-nvim",
     config = function()
       require "plugins.lspkind"
     end,
-  }
+  },
 
-  use {
+  {
     "hrsh7th/nvim-cmp",
-    requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-path", "hrsh7th/cmp-buffer", "saadparwaiz1/cmp_luasnip" },
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-path", "hrsh7th/cmp-buffer", "saadparwaiz1/cmp_luasnip" },
     config = function()
       require "plugins.cmp"
     end,
-  }
+  },
 
-  use {
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.lspconfig"
     end,
-  }
+  },
 
-  use {
+  {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
       require "plugins.null-ls"
     end,
-  }
+  },
 
-  use {
+  {
     "williamboman/mason.nvim",
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+          require "plugins.mason-lspconfig"
+        end,
+      },
+    },
     config = function()
       require "plugins.mason"
     end,
-  }
+  },
 
-  use {
-    "williamboman/mason-lspconfig.nvim",
-    after = "mason.nvim",
-    config = function()
-      require "plugins.mason-lspconfig"
-    end,
-  }
-
-  use {
+  {
     "folke/trouble.nvim",
     config = function()
       require "plugins.trouble"
     end,
-  }
+  },
 
-  use {
+  {
     "TimUntersberger/neogit",
     config = function()
       require "plugins.neogit"
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-lualine/lualine.nvim",
     config = function()
       require "plugins.lualine"
     end,
-  }
+  },
 
-  use {
+  {
     "numToStr/Comment.nvim",
     config = function()
       require "plugins.comment"
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-telescope/telescope.nvim",
     config = function()
       require "plugins.telescope"
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make",
-  }
+    build = "make",
+  },
 
-  use {
+  {
     "nvim-telescope/telescope-ui-select.nvim",
-  }
+  },
 
-  use {
+  {
     "folke/which-key.nvim",
     config = function()
       require "plugins.which-key"
     end,
-  }
+  },
 
-  use {
+  {
     "goolord/alpha-nvim",
     config = function()
       require "plugins.alpha"
     end,
-  }
-
-  if is_bootstrap then
-    require("packer").sync()
-  end
-end)
+  },
+})
