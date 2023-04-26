d2b141adb94f098cf8d2f1a7edda5c2ee8e53b8e
itsdalmo <kristian@dalmo.me>
2023-04-26 23:26:46 +0200

    Enable tab bar and fix fonts in wezterm

--- files/.config/wezterm/wezterm.lua
+++ files/.config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 local cfg = {}
 
@@ -6,19 +6,23 @@ local cfg = {}
 cfg.initial_cols = 180
 cfg.initial_rows = 48
 cfg.audible_bell = "Disabled"
-cfg.enable_tab_bar = false
+cfg.enable_tab_bar = true
 cfg.enable_scroll_bar = false
 cfg.window_close_confirmation = "NeverPrompt"
 cfg.window_padding = {
-  bottom = 0
+  bottom = 0,
 }
 
 -- Colors
 cfg.color_scheme = "tokyonight"
 
 -- Font
-cfg.font = wezterm.font("JetBrainsMono Nerd Font Mono", {
-  weight = "Medium"
+-- cfg.font = wezterm.font("JetBrainsMono Nerd Font Mono", {
+--   weight = "Medium",
+-- })
+cfg.font = wezterm.font_with_fallback({
+  { family = "JetBrains Mono", weight = "Medium" },
+  { family = "Symbols Nerd Font Mono", scale = 0.75 },
 })
 cfg.font_size = 13.0
 cfg.line_height = 1.2
