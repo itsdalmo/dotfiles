3dbfaa8b33e2e155cd0a0006b1095b0afa9efd57
itsdalmo <kristian@dalmo.me>
2024-03-04 23:37:40 +0100

    Pass --refresh to nix flake commands

--- files/.config/fish/functions/nix-flake-sync.fish
+++ files/.config/fish/functions/nix-flake-sync.fish
@@ -1,8 +1,8 @@
 # Sync flake.lock with what is currently being used by my dotfiles
 function nix-flake-sync
     set -l target "github:itsdalmo/dotfiles"
-    set -l metadata (nix flake metadata --json $target)
+    set -l metadata (nix flake metadata --refresh --json $target)
 
     set -l nixpkgs (echo $metadata | jq -r '.locks.nodes.nixpkgs.locked.rev')
-    nix flake update --override-input nixpkgs "github:nixos/nixpkgs/$nixpkgs"
+    nix flake update --refresh --override-input nixpkgs "github:nixos/nixpkgs/$nixpkgs"
 end
