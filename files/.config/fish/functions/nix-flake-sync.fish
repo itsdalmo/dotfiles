2275b26d05a49c3e4aae04bb3deff86f08a286bd
itsdalmo <kristian@dalmo.me>
2024-03-04 23:20:13 +0100

    Fix nix-flake-sync function

--- files/.config/fish/functions/nix-flake-sync.fish
+++ files/.config/fish/functions/nix-flake-sync.fish
@@ -4,9 +4,5 @@ function nix-flake-sync
     set -l metadata (nix flake metadata --json $target)
 
     set -l nixpkgs (echo $metadata | jq -r '.locks.nodes.nixpkgs.locked.rev')
-    set -l unstable (echo $metadata | jq -r '.locks.nodes.unstable.locked.rev')
-
-    nix flake update \
-        --override-input nixpkgs "github:nixos/nixpkgs/$nixpkgs" \
-        --override-input unstable "github:nixos/nixpkgs/$unstable"
+    nix flake update --override-input nixpkgs "github:nixos/nixpkgs/$nixpkgs"
 end
