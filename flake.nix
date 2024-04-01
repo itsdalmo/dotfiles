493a6e928fdc98ff26af034151f92c4fb717620a
itsdalmo <kristian@dalmo.me>
2024-04-02 00:39:42 +0200

    Switch back to following nixos-23.11
    Reverting xz on unstable takes ~10 days

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Nix as a package manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
