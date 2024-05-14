495212d00d50369e9db65e71365ed1abc2103b82
itsdalmo <kristian@dalmo.me>
2024-05-14 12:04:57 +0200

    wezterm: add shortcut to toggle horizontal split

--- files/.config/wezterm/wezterm.lua
+++ files/.config/wezterm/wezterm.lua
@@ -22,4 +22,28 @@ cfg.font_size = 13.0
 cfg.line_height = 1.2
 cfg.adjust_window_size_when_changing_font_size = false
 
+-- Keys
+cfg.keys = {
+  {
+    key = "Enter",
+    mods = "SUPER",
+    action = wezterm.action_callback(function(_, pane)
+      local tab = pane:tab()
+      local panes = tab:panes_with_info()
+      if #panes == 1 then
+        pane:split({
+          direction = "Bottom",
+          size = 0.4,
+        })
+      elseif not panes[1].is_zoomed then
+        panes[1].pane:activate()
+        tab:set_zoomed(true)
+      elseif panes[1].is_zoomed then
+        tab:set_zoomed(false)
+        panes[2].pane:activate()
+      end
+    end),
+  },
+}
+
 return cfg
