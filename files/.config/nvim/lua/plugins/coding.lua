6808d4b1461db5de5c07b207d6ba319d9b54c882
itsdalmo <kristian@dalmo.me>
2024-05-19 02:02:33 +0200

    nvim: use mini.notify

--- files/.config/nvim/lua/plugins/coding.lua
+++ files/.config/nvim/lua/plugins/coding.lua
@@ -38,6 +38,7 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
       "saadparwaiz1/cmp_luasnip",
     },
     opts = function()
@@ -68,6 +69,7 @@ return {
           { name = "luasnip" },
           { name = "buffer" },
           { name = "path" },
+          { name = "nvim_lsp_signature_help" },
         }),
         formatting = {
           format = function(_, item)
--- files/.config/nvim/lua/plugins/ui.lua
+++ files/.config/nvim/lua/plugins/ui.lua
@@ -11,27 +11,6 @@ return {
     end,
   },
 
-  -- ui components
-  { "MunifTanjim/nui.nvim", lazy = true },
-
-  -- better vim.ui
-  {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
-
   -- keybindings
   {
     "folke/which-key.nvim",
@@ -113,30 +92,8 @@ return {
     end,
   },
 
-  -- noicer ui
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-    },
-  },
+  -- notifications
+  { "echasnovski/mini.notify", event = "VeryLazy", config = true },
 
   -- statusline
   {
