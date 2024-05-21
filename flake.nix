47100e59a37be3a26cfdfaaf1f5ea285e542475d
itsdalmo <kristian@dalmo.me>
2024-05-21 15:54:48 +0200

    nix: update and install neovim from unstable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,23 +12,21 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
-      # Import nixpkgs with the correct config/overlays.
+      # Overlay some packages form unstable.
+      unstableOverlays = final: prev: {
+        neovim = (import inputs.nixpkgs-unstable { system = prev.system; config = prev.config; }).neovim;
+      };
+      # Helper function to create package sets
       mkPkgs = system: import nixpkgs {
         inherit system;
-        config = { allowUnfree = true; };
-        overlays = [ (import ./nix/overlays) ];
+        overlays = [ (import ./nix/overlays) unstableOverlays ];
       };
-
       # Create home-manager configuration for system/user.
-      mkHome = { system, user }:
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = mkPkgs system;
-          modules = [
-            ./home.nix
-          ];
-          extraSpecialArgs = { inherit user; };
-        };
-
+      mkHome = { system, user }: home-manager.lib.homeManagerConfiguration {
+        pkgs = mkPkgs system;
+        modules = [ ./home.nix ];
+        extraSpecialArgs = { inherit user; };
+      };
       # Helper function to add outputs for each supported system.
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f (mkPkgs system));
