2158afb747bf301b38fc25c9e4502e5d7dd8095e
itsdalmo <kristian@dalmo.me>
2024-12-05 16:42:08 +0100

    Use nightly version of wezterm

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,6 +1,7 @@
 # Overlay that adds my custom derivations to nixpkgs.
 inputs: final: prev: {
   unstable = (import inputs.nixpkgs-unstable { system = prev.system; config = prev.config; });
+  wezterm-nightly = (import inputs.wezterm-nightly { system = prev.system; config = prev.config; });
 
   # Custom packages
   tfcheck = final.callPackage ../pkgs/tfcheck { };
