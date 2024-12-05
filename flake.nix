2896cffff043284c61e90f06e05a0b57f009e498
itsdalmo <kristian@dalmo.me>
2024-12-05 10:17:09 +0100

    nix: update to 24.11

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.05";
+      url = "github:nixos/nixpkgs/nixos-24.11";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
