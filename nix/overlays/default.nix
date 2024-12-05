1f3f632e00661cb316ca7415f408bc80a8514abb
itsdalmo <kristian@dalmo.me>
2024-12-05 16:43:50 +0100

    Remove import

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,7 +1,7 @@
 # Overlay that adds my custom derivations to nixpkgs.
 inputs: final: prev: {
   unstable = (import inputs.nixpkgs-unstable { system = prev.system; config = prev.config; });
-  wezterm-nightly = (import inputs.wezterm-nightly { system = prev.system; config = prev.config; });
+  wezterm-nightly = inputs.wezterm-nightly;
 
   # Custom packages
   tfcheck = final.callPackage ../pkgs/tfcheck { };
