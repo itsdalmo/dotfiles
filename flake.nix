8b0a728bcef05be64dfe35011185af4e094ff2bf
itsdalmo <kristian@dalmo.me>
2024-12-07 11:03:26 +0100

    Try rolling back
    Revert "Fix moved packages"
    
    This reverts commit df147994fec5a7ee9e43f3320642587b1ff6208f.

--- flake.nix
+++ flake.nix
@@ -3,24 +3,18 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.11";
+      url = "github:nixos/nixpkgs/nixos-24.05";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Wezterm is broken on main: https://github.com/NixOS/nixpkgs/issues/336069
-    wezterm-nightly = {
-      url = "github:wez/wezterm?dir=nix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
