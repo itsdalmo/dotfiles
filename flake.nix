f56db138b5bbf67d20d0776ceb5e31803eb16312
itsdalmo <kristian@dalmo.me>
2024-12-07 11:24:38 +0100

    Revert "Try rolling back"
    This reverts commit a6524005550bc1e746a9d3ef80300d5b97dcfdef.

--- flake.nix
+++ flake.nix
@@ -3,18 +3,24 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.05";
+      url = "github:nixos/nixpkgs/nixos-24.11";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Wezterm is broken on main: https://github.com/NixOS/nixpkgs/issues/336069
+    wezterm-nightly = {
+      url = "github:wez/wezterm?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,6 +1,7 @@
 # Overlay that adds my custom derivations to nixpkgs.
 inputs: final: prev: {
   unstable = (import inputs.nixpkgs-unstable { system = prev.system; config = prev.config; });
+  wezterm-nightly = inputs.wezterm-nightly.packages.${prev.system}.default;
 
   # Custom packages
   tfcheck = final.callPackage ../pkgs/tfcheck { };
