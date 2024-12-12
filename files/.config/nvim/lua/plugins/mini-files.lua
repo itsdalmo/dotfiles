9e8cb3a155c21eb51a1dd681e221ace891598d37
itsdalmo <kristian@dalmo.me>
2024-12-12 10:18:44 +0100

    nvim: gy to yank path in mini.files

--- files/.config/nvim/lua/plugins/mini-files.lua
+++ files/.config/nvim/lua/plugins/mini-files.lua
@@ -30,12 +30,21 @@ local map_split = function(buf_id, lhs, direction)
   vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
 end
 
+local yank_path = function()
+  local path = (files.get_fs_entry() or {}).path
+  if path == nil then
+    return vim.notify("Cursor is not on valid entry")
+  end
+  vim.fn.setreg(vim.v.register, path)
+end
+
 vim.api.nvim_create_autocmd("User", {
   pattern = "MiniFilesBufferCreate",
   callback = function(args)
     local buf_id = args.data.buf_id
     map_split(buf_id, "<c-s>", "horizontal")
     map_split(buf_id, "<c-v>", "vertical")
+    vim.keymap.set("n", "gy", yank_path, { buffer = buf_id, desc = "Yank path" })
   end,
 })
 
