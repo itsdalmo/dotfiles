0d445df129fb569df4f7743e965bcab21376e67d
itsdalmo <kristian@dalmo.me>
2025-01-09 12:24:34 +0100

    nvim: reduce visual clutter in statusline

--- files/.config/nvim/lua/plugins/mini-statusline.lua
+++ files/.config/nvim/lua/plugins/mini-statusline.lua
@@ -1,15 +1,31 @@
+---@diagnostic disable: undefined-global
 local statusline = require("mini.statusline")
-statusline.section_filename = function()
-  return "%f%m%r"
+
+statusline.section_diff = function()
+  return ""
 end
+
+statusline.section_filename = function()
+  if vim.bo.buftype == "terminal" then
+    return "%t"
+  else
+    return "%f%m%r"
+  end
+end
+
+statusline.section_fileinfo = function()
+  local filetype = vim.bo.filetype
+  if filetype == "" then
+    return ""
+  end
+  return MiniIcons.get("filetype", filetype) .. " " .. filetype
+end
+
 statusline.section_location = function()
   return "%l:%v"
 end
-local inactive = function()
-  return "%#MiniStatuslineInactive#%f%m%r%="
-end
+
 return statusline.setup({
-  content = { inactive = inactive },
   use_icons = true,
   set_vim_settings = true,
 })
