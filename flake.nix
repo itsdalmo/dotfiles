8736a51be6c53bac58ac2bfe2742239ade7033d9
itsdalmo <kristian@dalmo.me>
2025-01-16 22:28:26 +0100

    nix: bump all the things

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
