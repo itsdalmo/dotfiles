a139e61aa35e3c183aa2e3ffdd339b529ebacab0
itsdalmo <kristian@dalmo.me>
2025-02-26 09:34:30 +0100

    nvim: ignore exit code of golangcilint

--- files/.config/nvim/lua/plugins/lint.lua
+++ files/.config/nvim/lua/plugins/lint.lua
@@ -7,6 +7,8 @@ lint.linters_by_ft = {
   proto = { "buf_lint" },
   sh = { "shellcheck" },
 }
+-- NOTE: Always exists with code 7 (presumably because it can't locate the config?)
+lint.linters.golangcilint.ignore_exitcode = true
 
 local group = vim.api.nvim_create_augroup("lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
