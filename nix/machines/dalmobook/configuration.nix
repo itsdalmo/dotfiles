65ba61c2fe73310066c071651fdd4ae4e92345ed
itsdalmo <kristian@dalmo.me>
2025-08-01 13:35:07 +0200

    dalmobook: add static dns servers

--- nix/machines/dalmobook/configuration.nix
+++ nix/machines/dalmobook/configuration.nix
@@ -46,6 +46,15 @@ in
     hostName = hostName;
     computerName = hostName;
     localHostName = hostName;
+
+    dns = [
+      "8.8.8.8"
+      "8.8.4.4"
+      "2001:4860:4860::8888"
+      "2001:4860:4860::8844"
+    ];
+
+    knownNetworkServices = [ "Wi-Fi" ];
   };
 
   homebrew.masApps = {
